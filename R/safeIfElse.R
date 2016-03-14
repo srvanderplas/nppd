3aeb79763a97102a2717e8bd0fab48148745ba53
Susan Vanderplas <S1Vande@nppd.com>
2016-03-14 07:48:50 -0500

    safe.ifelse function from StackOverflow

---
+++ R/safeIfElse.R
@@ -0,0 +1,9 @@
+#' Safe (type maintained) version of ifelse
+#' @param cond logical condition
+#' @param yes replacement if condition is true
+#' @param no replacement if condition is false
+#' @details From http://stackoverflow.com/questions/6668963/how-to-prevent-ifelse-from-turning-date-objects-into-numeric-objects
+#' @export
+safe.ifelse <- function(cond, yes, no) {
+  structure(ifelse(cond, yes, no), class = class(yes))
+}
